703fad25d67fd6e5225f287eab74421b15cd2d34
Unknown <60070127@kmitl.ac.th>
2017-10-05 14:54:31 +0700

    Menu
    tell the menu

---
+++ menu list.py
@@ -0,0 +1,21 @@
+"""menu R/"""
+
+{
+'Khao Phad Mhoo':['rice','pork','egg'],
+'Khao Phad Mhoo':['rice','chicken','egg'],
+'Mhoo Phad Kratiem':['pork','garlic'],
+'Ghai Phad Kratiem':['chicken','garlic'],
+'Mhoo Phad Kra Phrao':['pork','basil'],
+'Ghai Phad Kra Phrao':['chicken','basil'],
+'Mama Phad Mhoo':['mama','pork','egg'],
+'Mama Phad Ghai':['mama','chicken','egg'],
+'Mhoo Phad Nam Plar':['pork','fish sauce'],
+'Ghai Phad Nam Plar':['chicken','fish sauce'],
+'Panaeng Mhoo':['pork','panaeng curry','coconut milk','red chilli'],
+'Panaeng Ghai':['chicken','panaeng curry','coconut milk','red chilli'],
+'Fuck Tong Phad Khai':['pumpkin','egg','garlic'],
+'Khai Palo':['egg','tofu','palo','garlic']
+}
+
+
+
