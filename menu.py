9640a3af21929c4b5ace1c5cb1bc1c270a6a87c9
Kate <60070127@kmitl.ac.th>
2017-10-05 23:57:08 +0700

    Menu
    add 15-19

--- menu.py
+++ menu.py
@@ -1,14 +1,19 @@
-'Khao Phad Mhoo':['rice','pork','egg'],
-'Khao Phad Mhoo':['rice','chicken','egg'],
+'Khao Phad Mhoo':['rice','pork','egg','soy sauce'],
+'Khao Phad Mhoo':['rice','chicken','egg','soy sauce'],
 'Mhoo Phad Kratiem':['pork','garlic'],
 'Ghai Phad Kratiem':['chicken','garlic'],
-'Mhoo Phad Kra Phrao':['pork','basil'],
-'Ghai Phad Kra Phrao':['chicken','basil'],
+'Mhoo Phad Kra Phrao':['pork','basil','chili','garlic','oyster sauce'],
+'Ghai Phad Kra Phrao':['chicken','basil','chili','garlic','oyster sauce'],
 'Mama Phad Mhoo':['mama','pork','egg'],
 'Mama Phad Ghai':['mama','chicken','egg'],
-'Mhoo Phad Nam Plar':['pork','fish sauce'],
-'Ghai Phad Nam Plar':['chicken','fish sauce'],
+'Mhoo Thord Nam Plar':['pork','fish sauce'],
+'Ghai Thord Nam Plar':['chicken','fish sauce'],
 'Panaeng Mhoo':['pork','panaeng curry','coconut milk','red chilli'],
 'Panaeng Ghai':['chicken','panaeng curry','coconut milk','red chilli'],
 'Fuck Tong Phad Khai':['pumpkin','egg','garlic'],
 'Khai Palo':['egg','tofu','palo','garlic']
+'Khai Jiew Mhoo Sahb':['egg','pork']
+'Mhoo Phad Nammun Hoy':['pork','oyster sauce','garlic']
+'Mhoo Phad Phonk Kraree':['pork','curry powder','onion','milk','egg']
+'Phad Prick Gaenk Mhoo':['pork','curry',]
+'Phad Phonk Kraree Thale':['squid','shrimp','onion','milk','egg','curry powder']
