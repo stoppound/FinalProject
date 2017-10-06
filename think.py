ecd97e7fb32443683e6fb246824a08eebeb389ae
Unknown <60070143@kmitl.ac.th>
2017-10-06 22:57:31 +0700

    think(update1)
    อย่าพึ่งทำไรกับไฟล์thinkนะ เขต

--- think.py
+++ think.py
@@ -1,24 +1,44 @@
 """think"""
 def data(thing):
     menu = {'Khao Phad Mhoo':['rice','pork','egg'],
-            'Khao Phad Mhoo':['rice','chicken','egg'],
+            'Khao Phad kai':['rice','chicken','egg'],
             'Mhoo Phad Kratiem':['pork','garlic'],
-            'Ghai Phad Kratiem':['chicken','garlic'],
+            'kai Phad Kratiem':['chicken','garlic'],
             'Mhoo Phad Kra Phrao':['pork','basil'],
-            'Ghai Phad Kra Phrao':['chicken','basil'],
+            'kai Phad Kra Phrao':['chicken','basil'],
             'Mama Phad Mhoo':['mama','pork','egg'],
-            'Mama Phad Ghai':['mama','chicken','egg'],
+            'Mama Phad kai':['mama','chicken','egg'],
             'Mhoo Phad Nam Plar':['pork','fish sauce'],
-            'Ghai Phad Nam Plar':['chicken','fish sauce'],
+            'kai Phad Nam Plar':['chicken','fish sauce'],
             'Panaeng Mhoo':['pork','panaeng curry','coconut milk','red chilli'],
-            'Panaeng Ghai':['chicken','panaeng curry','coconut milk','red chilli'],
-            'Fuck Tong Phad Khai':['pumpkin','egg','garlic'],
+            'Panaeng kai':['chicken','panaeng curry','coconut milk','red chilli'],
+            'Fakthong Phad Khai':['pumpkin','egg','garlic'],
             'Khai Palo':['egg','tofu','palo','garlic']
             }
-    for i in thing:
-        for var in menu:
-            if i in menu[var]:
-                print(menu[var])
+    lst1 = []
+    lst2 = []
+    lst3 = []
+    lst4 = []
+    for m in menu:
+        num_count = 0
+        for i in thing:
+            if i in menu[m]:
+                num_count += 1
+        lst1.append((len(menu[m])-num_count, m))
+    lst1.sort()
+    # for i in thing:
+    #     for m in menu:
+    #         if m not in lst1:
+    #             if i in menu[m]:
+    #                
+    output(lst1)
+
+
+def output(out):
+    for i in out:
+        print(i)
+
+
 def main():
     check = input()
     thing = []
