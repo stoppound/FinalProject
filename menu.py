069846beed99063c1853dd299f0b7e70616065f4
Kate <60070127@kmitl.ac.th>
2017-10-07 12:54:03 +0700

    Menu
    add new menu(19-23)

--- menu.py
+++ menu.py
@@ -2,8 +2,8 @@
 'Khao Phad Mhoo':['rice','chicken','egg','soy sauce'],
 'Mhoo Phad Kratiem':['pork','garlic'],
 'Ghai Phad Kratiem':['chicken','garlic'],
-'Mhoo Phad Kra Phrao':['pork','basil','chili','garlic','oyster sauce'],
-'Ghai Phad Kra Phrao':['chicken','basil','chili','garlic','oyster sauce'],
+'Mhoo Phad Kra Phrao':['pork','basil','chilli','garlic','oyster sauce'],
+'Ghai Phad Kra Phrao':['chicken','basil','chilli','garlic','oyster sauce'],
 'Mama Phad Mhoo':['mama','pork','egg'],
 'Mama Phad Ghai':['mama','chicken','egg'],
 'Mhoo Thord Nam Plar':['pork','fish sauce'],
@@ -17,3 +17,7 @@
 'Mhoo Phad Phonk Kraree':['pork','curry powder','onion','milk','egg']
 'Phad Prick Gaenk Mhoo':['pork','curry',]
 'Phad Phonk Kraree Thale':['squid','shrimp','onion','milk','egg','curry powder']
+'Khaw Thom Mhoo':['pork','rice']
+'Khaw Thom Koong':['shrimp','rice']
+'Masman Ghai':['chicken','onion','coconut milk','potato','masman curry']
+'Mhoo Manaw':['pork','garlic','red chilli','fish sauce','lemon']
